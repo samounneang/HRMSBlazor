aaa47104784f6d5b54db7e64b5325248bde23d6c
Sam Oun Neang <somoun.neang@gmail.com>
2024-10-20 19:46:28 +0700

    add layout radzen

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
 using BlazorApp9;
+using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.RootComponents.Add
+//builder.HostEnvironment
+//builder.Logging
+//builder.Configuration
+//builder.ConfigureContainer
+//builder.Services
+//builder.
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Logging.SetMinimumLevel(LogLevel.Warning); // Log only warnings and above
+//builder.Logging.AddProvider();
+builder.Services.AddRadzenComponents();
+
 
 await builder.Build().RunAsync();
+
